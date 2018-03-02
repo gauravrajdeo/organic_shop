0ebf8d1489a29778bbd3e6ee4eec3c8c0391a48a
Gaurav Rajdeo <rajdeogaurav@gmail.com>
2018-03-02 16:27:20 +0530

    new components added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBujhh96sU3NqwyEmZLPIc2FyAMX0wUPsg",
+    authDomain: "oshop-6119e.firebaseapp.com",
+    databaseURL: "https://oshop-6119e.firebaseio.com",
+    projectId: "oshop-6119e",
+    storageBucket: "oshop-6119e.appspot.com",
+    messagingSenderId: "258799267905"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBujhh96sU3NqwyEmZLPIc2FyAMX0wUPsg",
+    authDomain: "oshop-6119e.firebaseapp.com",
+    databaseURL: "https://oshop-6119e.firebaseio.com",
+    projectId: "oshop-6119e",
+    storageBucket: "oshop-6119e.appspot.com",
+    messagingSenderId: "258799267905"
+  }
 };
